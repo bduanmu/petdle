865af5959ff0e643a1d3dfbf14fe4f26878733e2
bduanmu <74875982+bduanmu@users.noreply.github.com>
2023-12-25 15:38:31 -0500

    Hints now work

--- src/App.jsx
+++ src/App.jsx
@@ -6,24 +6,70 @@ import enterArrow from "./assets/Enter_Arrow.png"
 import { pets, petNames } from "./pets.jsx";
 
 const correctPetIndex = Math.floor(Math.random() * petNames.length)
+const correctPet = pets[correctPetIndex]
+
+const WRONG_COLOUR = ["#676767", "#414141"]
+const CLOSE_COLOUR = ["#EDB82E", "#CB8F35"]
+const CORRECT_COLOUR = ["#32ED2E", "#2CA02A"]
+
 console.log(petNames[correctPetIndex])
 
 export default function App() {
   const [newGuess, setNewGuess] = useState('')
   const [guessedPets, setGuessedPets] = useState([])
+  const [hints, setHints] = useState([])
 
   function handleGuess(e) {
     e.preventDefault()
 
     const petIndex = petNames.indexOf(newGuess)
 
-    if (petNames.includes(newGuess) && !guessedPets.includes(petIndex)) {
+    if (petIndex != -1 && !guessedPets.includes(petIndex)) {
       setGuessedPets(currentGuess => {
-        return [...currentGuess, petIndex]
+        return [petIndex, ...currentGuess]
       })
+
+      const hint = {
+        "pet": pets[petIndex], 
+        "pet_colour": WRONG_COLOUR, 
+        "tier": WRONG_COLOUR, 
+        "pack": WRONG_COLOUR,
+        "attack": WRONG_COLOUR, 
+        "health": WRONG_COLOUR, 
+        "ability": WRONG_COLOUR
+      }
+      
       if (petIndex == correctPetIndex) {
         console.log("win")
+        hint.pet_colour = CORRECT_COLOUR
       }
+      if (hint.pet.tier == correctPet.tier) {
+        hint.tier = CORRECT_COLOUR
+      } else if (Math.abs(hint.pet.tier - correctPet.tier) == 1) {
+        hint.tier = CLOSE_COLOUR
+      }
+      if (JSON.stringify(hint.pet.pack.toSorted()) == JSON.stringify(correctPet.pack.toSorted())) {
+        hint.pack = CORRECT_COLOUR
+      } else if (hint.pet.pack.some(r => correctPet.pack.includes(r))) {
+        hint.pack = CLOSE_COLOUR
+      }
+      if (hint.pet.attack == correctPet.attack) {
+        hint.attack = CORRECT_COLOUR
+      } else if (Math.abs(hint.pet.attack - correctPet.attack) == 1) {
+        hint.attack = CLOSE_COLOUR
+      }
+      if (hint.pet.health == correctPet.health) {
+        hint.health = CORRECT_COLOUR
+      } else if (Math.abs(hint.pet.health - correctPet.health) == 1) {
+        hint.health = CLOSE_COLOUR
+      }
+      if (hint.pet.ability == correctPet.ability) {
+        hint.ability = CORRECT_COLOUR
+      }
+
+      setHints(currentHints => {
+        return [hint, ...currentHints]
+      })
 
       setNewGuess("")
     }
@@ -79,48 +125,48 @@ export default function App() {
               <label>Ability</label>
             </div>
           </li>
-          {guessedPets.map(petIndex => {
+          {hints.map(hint => {
             return (
-              <li key={petIndex} className="guessListObject">
+              <li key={hint.pet.name} className="guessListObject">
                 <div className="infoBox">
-                  <div className="infoBoxOuter">
-                    <div className="infoBoxInner" style={{fontSize: 20}}>
-                      <label>{pets[petIndex].name}</label>
+                  <div className="infoBoxOuter" style={{backgroundColor: hint.pet_colour[1]}}>
+                    <div className="infoBoxInner" style={{fontSize: 20, backgroundColor: hint.pet_colour[0]}}>
+                      <label>{hint.pet.name}</label>
                     </div>
                   </div>
                 </div>
                 <div className="infoBox">
-                  <div className="infoBoxOuter">
-                    <div className="infoBoxInner" style={{fontSize: 40}}>
-                      <label>{pets[petIndex].tier}</label>
+                  <div className="infoBoxOuter" style={{backgroundColor: hint.tier[1]}}>
+                    <div className="infoBoxInner" style={{fontSize: 40, backgroundColor: hint.tier[0]}}>
+                      <label>{hint.pet.tier}</label>
                     </div>
                   </div>
                 </div>
                 <div className="infoBox">
-                  <div className="infoBoxOuter">
-                    <div className="infoBoxInner" style={{fontSize: 20}}>
-                      <label>{pets[petIndex].pack.join(', ')}</label>
+                  <div className="infoBoxOuter" style={{backgroundColor: hint.pack[1]}}>
+                    <div className="infoBoxInner" style={{fontSize: 20, backgroundColor: hint.pack[0]}}>
+                      <label>{hint.pet.pack.join(', ')}</label>
                     </div>
                   </div>
                 </div>
                 <div className="infoBox">
-                  <div className="infoBoxOuter">
-                    <div className="infoBoxInner" style={{fontSize: 40}}>
-                      <label>{pets[petIndex].attack}</label>
+                  <div className="infoBoxOuter" style={{backgroundColor: hint.attack[1]}}>
+                    <div className="infoBoxInner" style={{fontSize: 40, backgroundColor: hint.attack[0]}}>
+                      <label>{hint.pet.attack}</label>
                     </div>
                   </div>
                 </div>
                 <div className="infoBox">
-                  <div className="infoBoxOuter">
-                    <div className="infoBoxInner" style={{fontSize: 40}}>
-                      <label>{pets[petIndex].health}</label>
+                  <div className="infoBoxOuter" style={{backgroundColor: hint.health[1]}}>
+                    <div className="infoBoxInner" style={{fontSize: 40, backgroundColor: hint.health[0]}}>
+                      <label>{hint.pet.health}</label>
                     </div>
                   </div>
                 </div>
                 <div className="infoBox">
-                  <div className="infoBoxOuter">
-                    <div className="infoBoxInner">
-                      <label className="abilityLabel">{pets[petIndex].ability}</label>
+                  <div className="infoBoxOuter" style={{backgroundColor: hint.ability[1]}}>
+                    <div className="infoBoxInner" style={{backgroundColor: hint.ability[0]}}>
+                      <label className="abilityLabel">{hint.pet.ability}</label>
                     </div>
                   </div>
                 </div>
